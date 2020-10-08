cc10afcea9db807ba8c7fcd5875300b0d05429ec
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2020-10-08 13:59:21 +0100

    final

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -52,7 +52,19 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    try {
+      const response = await api.put(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+      setFoods(
+        foods.map(mappedFood =>
+          mappedFood.id === editingFood.id ? { ...response.data } : mappedFood,
+        ),
+      );
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
@@ -74,7 +86,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    toggleModal();
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
